e5b2bfeb44b08872eb9b5233bb6413387504d1a2
Greggy Gianini Firmansyah <greggygf@gmail.com>
2020-05-11 19:30:23 +0700

    add font

---
+++ app/src/main/java/com/bluohazard/spk_pemilihan_tahsin_kota_malang_apps/ResultActivity.java
@@ -0,0 +1,14 @@
+package com.bluohazard.spk_pemilihan_tahsin_kota_malang_apps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
