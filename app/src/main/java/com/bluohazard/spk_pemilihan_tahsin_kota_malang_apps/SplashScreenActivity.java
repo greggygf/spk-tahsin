fadb3a26df6f7752c4d7aad146db4f8ac269494c
Greggy Gianini Firmansyah <greggygf@gmail.com>
2020-05-11 19:51:16 +0700

    splash screen done

--- app/src/main/java/com/bluohazard/spk_pemilihan_tahsin_kota_malang_apps/SplashScreenActivity.java
+++ app/src/main/java/com/bluohazard/spk_pemilihan_tahsin_kota_malang_apps/SplashScreenActivity.java
@@ -1,14 +1,25 @@
-package com.bluohazard.spk_pemilihan_tahsin_kota_malang_apps;
+ package com.bluohazard.spk_pemilihan_tahsin_kota_malang_apps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
-public class SplashScreenActivity extends AppCompatActivity {
+ public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
     }
 }
